07ef7c8b1a5e0ab01052991cba9361222f7abd68
Apollo Tang <tang.apollo@gmail.com>
2021-09-13 23:03:37 -0400

    [1a] share utils package works in both apps

--- 1a--add-node-app/apps/web-app-1/src/app/index.tsx
+++ 1a--add-node-app/apps/web-app-1/src/app/index.tsx
@@ -5,9 +5,15 @@ import s from './style.module.less';
 import Component1 from '@myscope/ui--component-1';
 
 import FontTest from '../font-test/';
+
+import {fnInFoo1} from "@myscope/util--foo-1"
+
+const someString:string = fnInFoo1(23)
+
 const App = () => (
   <div className={`${s.app}`}>
     <h1>Hello React</h1>
+    <div>someString: {someString}</div>
     {/* <div><MyButton>mybutton</MyButton> </div> */}
     <div className={`${s.imageContainer}`}><img src={img} /></div>
     <Component1 />
